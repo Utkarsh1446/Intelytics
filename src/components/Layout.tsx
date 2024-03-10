834e1bf93297d2809508ddc7835e781dc4beac72
Utkarsh1446 <100139597+Utkarsh1446@users.noreply.github.com>
2024-03-11 00:00:56 +0530

    Fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Image from "next/image";
 import Logo from "./assets/logo.png";
+import Link from "next/link";
 const Layout = ({ children }: any) => {
   const [isVisibie, setIsVisible] = React.useState<boolean>(false);
   return (
@@ -10,11 +11,13 @@ const Layout = ({ children }: any) => {
         <Sidebar visible={isVisibie} setVisible={setIsVisible} />
         <div className="  bg-slate-950 flex-1 px-6 pt-8 pb-4 text-white border-1 border-dashed overflow-y-scroll">
           <div className="lg:hidden bg-slate-950 w-full pt-0 pb-5 flex items-center justify-between">
-            <Image
-              className="w-[10rem] h-[1.5rem]"
-              src={Logo}
-              alt="Intelitics logo"
-            />
+            <Link href={"/"}>
+              <Image
+                className="w-[10rem] h-[1.5rem]"
+                src={Logo}
+                alt="Intelitics logo"
+              />
+            </Link>
             <button
               className="rotate-90 text-3xl select-none hover:text-gray-400"
               onClick={() => setIsVisible(true)}
